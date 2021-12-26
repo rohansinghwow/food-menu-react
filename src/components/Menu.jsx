61b0b056ef7d515e1310d86d095f72a39bdfee7f
rohansinghwow <rohansinghwow@gmail.com>
2021-12-26 23:15:12 +0530

    Adding category

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,14 +4,29 @@ import data from '../data'
 
 export default function Menu(){
 
+    const [cat,setCat] = React.useState([... new Set(data.map(item=>item.category))])
+
+    const catMapper = cat.map(item=>{
+        
+           return <button className="px-4 w-32 capitalize py-2 bg-amber-700 font-medium text-gray-50">{item}</button>
+        
+    })
+    
+    
+
     const mapper = data.map(item=>{
         return (
-            <Dish key={item.id} title={item.title} img={item.img} desc={item.desc} price={item.price} cat={item.category}/>
+            
+            <Dish key={item.id} title={item.title} img={item.img} desc={item.desc} price={item.price}/>
+
         )
     })
 
     return (
         <div className=" flex flex-wrap justify-start align-center flex-row">
+            <div className="category container text-center mb-5">
+                {catMapper}
+            </div>
                 {mapper}
         </div>
         
